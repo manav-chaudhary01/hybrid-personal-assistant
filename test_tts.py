3ac5fce9b6756838065f229f8df1953e4c1ab2df
Manav Chaudhary <manav.chaudhary1303@gmail.com>
2026-04-19 17:50:28 +0530

    cleanup: remove unnecessary tracked files

--- test_tts.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init('sapi5')
-engine.setProperty('rate', 180)
-engine.setProperty('volume', 1.0)
-
-engine.say("If you hear this, TTS is working")
-engine.runAndWait()
